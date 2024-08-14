a38ff44182afb5b90dc3f5677ad290c95e22d3f6
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-14 21:36:03 +0300

    created a FullInput component with useState hook

---
+++ src/common/components/FullInput/FullInput.tsx
@@ -0,0 +1,21 @@
+import {ChangeEvent, useState} from "react";
+import {FullInputProps} from "common/types/FullInput/FullInputProps.ts";
+import {Button} from "common/components/Button/Button.tsx";
+
+export const FullInput = (props: FullInputProps) => {
+    const [value, setValue] = useState("");
+    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.currentTarget.value)
+    }
+    const onClickButtonHandler = () => {
+        props.onChange && props.onChange(value)
+        setValue('')
+    }
+
+    return (
+        <>
+            <input type="text" value={value} onChange={onChangeInputHandler}/>
+    <Button name="+" onClick={onClickButtonHandler}/>
+    </>
+);
+};
