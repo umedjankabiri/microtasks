ce1e5ac527aec05492c8b4dead266c9306bf6b2a
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 01:39:34 +0300

    Replace StrictMode with BrowserRouter and RoutingApp
    This change integrates React Router by replacing StrictMode with BrowserRouter and RoutingApp components. This setup is necessary to enable client-side routing within the application. Import statements were also adjusted to accommodate these changes.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import {createRoot} from 'react-dom/client'
 import './index.css'
+import {BrowserRouter} from "react-router-dom";
+import {RoutingApp} from "common/components/Routing/RoutingApp.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <RoutingApp />
+  </BrowserRouter>,
 )
