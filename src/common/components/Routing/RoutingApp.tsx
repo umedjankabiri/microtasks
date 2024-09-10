0ed231c117abc070196d78bca7b4ea89f56165bd
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 02:30:06 +0300

    Enhance navigation and update routing components
    Added NavLink and Link components for better navigation and updated error handling with a new Error404 component. Enhanced the routing logic by using Navigate for redirects and improved user experience by dynamically applying CSS styles to active navigation links.

--- src/common/components/Routing/RoutingApp.tsx
+++ src/common/components/Routing/RoutingApp.tsx
@@ -1,9 +1,9 @@
 import stl from "common/components/Routing/site.module.css"
-import {Route, Routes} from "react-router-dom";
+import {Link, Navigate, NavLink, Route, Routes} from "react-router-dom";
 import {PageOne} from "common/components/Routing/Pages/PageOne.tsx";
 import {PageTwo} from "common/components/Routing/Pages/PageTwo.tsx";
 import {PageThree} from "common/components/Routing/Pages/PageThree.tsx";
-import {ErrorPage} from "common/components/Routing/Error/ErrorPage.tsx";
+import {Error404} from "common/components/Routing/Error/Error404.tsx";
 
 export const RoutingApp = () => {
 
@@ -12,18 +12,41 @@ export const RoutingApp = () => {
             <div className={stl.header}><h1>HEADER</h1></div>
             <div className={stl.body}>
                 <div className={stl.nav}>
-                    Здесь будет навигация
+                    <div>
+                        <NavLink
+                            className={({isActive}) => isActive ? stl.activeNavLink : stl.navLink}
+                            to={"/Page1"}
+                        >Adidas</NavLink>
+                    </div>
+                    <div>
+                        <NavLink
+                            className={({isActive}) => isActive ? stl.activeNavLink : stl.navLink}
+                            to={"/Page2"}
+                        >Bershka</NavLink>
+                    </div>
+                    <div>
+                        <NavLink
+                            className={({isActive}) => isActive ? stl.activeNavLink : stl.navLink}
+                            to={"/Page3"}
+                        >Pull&Bear</NavLink>
+                    </div>
+                    <div>
+                        <Link to={"/Page3"}>Pull&Bear</Link>
+                    </div>
+                    <div>
+                        <a href={"/Page3"}>Pull&Bear with tah a</a>
+                    </div>
                 </div>
                 <div className={stl.content}>
-                        <Routes>
-                            <Route path={"/"} element={<PageOne/>}/>
-                            <Route path={"/Page1"} element={<PageOne/>}/>
-                            <Route path={"/Page2"} element={<PageTwo/>}/>
-                            <Route path={"/Page3"} element={<PageThree/>}/>
+                    <Routes>
+                        <Route path={"/"} element={<Navigate to={"/Page1"}/>}/>
+                        <Route path={"/Page1"} element={<PageOne/>}/>
+                        <Route path={"/Page2"} element={<PageTwo/>}/>
+                        <Route path={"/Page3"} element={<PageThree/>}/>
 
-                            <Route path={"/*"} element={<ErrorPage/>}/>
-                            <Route path={"/Error"} element={<ErrorPage/>}/>
-                        </Routes>
+                        <Route path={"/Error"} element={<Error404/>}/>
+                        <Route path={"/*"} element={<Navigate to={"/error404"}/>}/>
+                    </Routes>
                 </div>
             </div>
             <div className={stl.footer}>abibas 2023</div>
