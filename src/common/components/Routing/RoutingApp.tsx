184cf1e6b7001a328e4467b49a53f90b96c94769
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 02:59:13 +0300

    Refactor nav links with styled-components
    Replaced repetitive NavLink elements with a reusable NavWrapper styled component for better maintainability. Adjusted some styling directly within NavWrapper to simplify the component structure and ensure consistent link behavior.

--- src/common/components/Routing/RoutingApp.tsx
+++ src/common/components/Routing/RoutingApp.tsx
@@ -4,6 +4,7 @@ import {PageOne} from "common/components/Routing/Pages/PageOne.tsx";
 import {PageTwo} from "common/components/Routing/Pages/PageTwo.tsx";
 import {PageThree} from "common/components/Routing/Pages/PageThree.tsx";
 import {Error404} from "common/components/Routing/Error/Error404.tsx";
+import styled from "styled-components";
 
 export const RoutingApp = () => {
 
@@ -12,29 +13,20 @@ export const RoutingApp = () => {
             <div className={stl.header}><h1>HEADER</h1></div>
             <div className={stl.body}>
                 <div className={stl.nav}>
+                    <NavWrapper>
+                        <NavLink to={"/Page1"}>Adidas</NavLink>
+                    </NavWrapper>
+                    <NavWrapper>
+                        <NavLink to={"/Page2"}>Bershka</NavLink>
+                    </NavWrapper>
+                    <NavWrapper>
+                        <NavLink to={"/Page3"}>Pull&Bear</NavLink>
+                    </NavWrapper>
                     <div>
-                        <NavLink
-                            className={({isActive}) => isActive ? stl.activeNavLink : stl.navLink}
-                            to={"/Page1"}
-                        >Adidas</NavLink>
+                        <Link to={"/Page3"}>Pull&Bear with tag Link</Link>
                     </div>
                     <div>
-                        <NavLink
-                            className={({isActive}) => isActive ? stl.activeNavLink : stl.navLink}
-                            to={"/Page2"}
-                        >Bershka</NavLink>
-                    </div>
-                    <div>
-                        <NavLink
-                            className={({isActive}) => isActive ? stl.activeNavLink : stl.navLink}
-                            to={"/Page3"}
-                        >Pull&Bear</NavLink>
-                    </div>
-                    <div>
-                        <Link to={"/Page3"}>Pull&Bear</Link>
-                    </div>
-                    <div>
-                        <a href={"/Page3"}>Pull&Bear with tah a</a>
+                        <a href={"/Page3"}>Pull&Bear with tag a</a>
                     </div>
                 </div>
                 <div className={stl.content}>
@@ -53,3 +45,20 @@ export const RoutingApp = () => {
         </div>
     );
 };
+
+const NavWrapper = styled.div `
+    margin-left: 10px;
+    font-size: 20px;
+    
+    & > a {
+        text-decoration: none;
+        color: #1e3786;
+    }
+    & > a.active {
+        text-decoration: none;
+        color: #03eaff;
+    }
+    & > a:hover {
+        color: steelblue;
+    }
+`
