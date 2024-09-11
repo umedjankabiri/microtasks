a305e67a9864641abc64fac696c5fead9a0d120c
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 03:02:09 +0300

    Add styling for navigation wrapper in Routing component
    Created a new styled-component `NavWrapper` to handle the navigation element styling in the Routing module. This includes default, active, and hover states for anchor tags within the navigation wrapper.

---
+++ src/common/components/Routing/_styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const NavWrapper = styled.div `
+    margin-left: 10px;
+    font-size: 20px;
+    
+    & > a {
+        text-decoration: none;
+        color: #1e3786;
+    }
+    & > a.active {
+        text-decoration: none;
+        color: #03eaff;
+    }
+    & > a:hover {
+        color: steelblue;
+    }
+`
