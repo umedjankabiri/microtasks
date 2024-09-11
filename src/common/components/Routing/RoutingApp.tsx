0eed693ea60fe3b79b81af61bbfb6bf788c90c83
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 03:37:00 +0300

    Refactor to use constants for route paths
    Introduced a PATH constant object for route paths to eliminate hardcoded strings in the RoutingApp component. This improves maintainability and reduces potential errors from mistyped paths. Adjusted all relevant links and routes to use the defined constants.

--- src/common/components/Routing/RoutingApp.tsx
+++ src/common/components/Routing/RoutingApp.tsx
@@ -5,6 +5,13 @@ import {PageTwo} from "common/components/Routing/Pages/PageTwo.tsx";
 import {PageThree} from "common/components/Routing/Pages/PageThree.tsx";
 import {Error404} from "common/components/Routing/Error/Error404.tsx";
 
+const PATH = {
+    PAGE1: '/page1',
+    PAGE2: '/page2',
+    PAGE3: '/page3',
+    ERROR404: '/error404'
+} as const
+
 export const RoutingApp = () => {
 
     return (
@@ -15,36 +22,36 @@ export const RoutingApp = () => {
                     <div>
                         <NavLink
                             className={({isActive}) => isActive ? stl.activeNavLink : stl.navLink}
-                            to={"/Page1"}
+                            to={PATH.PAGE1}
                         >Adidas</NavLink>
                     </div>
                     <div>
                         <NavLink
                             className={({isActive}) => isActive ? stl.activeNavLink : stl.navLink}
-                            to={"/Page2"}
+                            to={PATH.PAGE2}
                         >Bershka</NavLink>
                     </div>
                     <div>
                         <NavLink
                             className={({isActive}) => isActive ? stl.activeNavLink : stl.navLink}
-                            to={"/Page3"}
+                            to={PATH.PAGE3}
                         >Pull&Bear</NavLink>
                     </div>
                     <div>
-                        <Link to={"/Page3"}>Pull&Bear with tag Link</Link>
+                        <Link to={PATH.PAGE3}>Pull&Bear with tag Link</Link>
                     </div>
                     <div>
-                        <a href={"/Page3"}>Pull&Bear with tag a</a>
+                        <a href={PATH.PAGE3}>Pull&Bear with tag a</a>
                     </div>
                 </div>
                 <div className={stl.content}>
                     <Routes>
                         <Route path={"/"} element={<Navigate to={"/Page1"}/>}/>
-                        <Route path={"/Page1"} element={<PageOne/>}/>
-                        <Route path={"/Page2"} element={<PageTwo/>}/>
-                        <Route path={"/Page3"} element={<PageThree/>}/>
+                        <Route path={PATH.PAGE1} element={<PageOne/>}/>
+                        <Route path={PATH.PAGE2} element={<PageTwo/>}/>
+                        <Route path={PATH.PAGE3} element={<PageThree/>}/>
 
-                        <Route path={"/Error"} element={<Error404/>}/>
+                        <Route path={PATH.ERROR404} element={<Error404/>}/>
                         <Route path={"/*"} element={<Navigate to={"/error404"}/>}/>
                     </Routes>
                 </div>
