d6dc8901a5cb4871bf00b25bcf95d927233dbb63
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 03:07:37 +0300

    Refactor RoutingApp NavLinks for better styling
    Replaced custom NavWrapper with divs for nav links and added conditional class names for active state styling. This simplifies the structure and ensures that active NavLinks are highlighted appropriately.

--- src/common/components/Routing/RoutingApp.tsx
+++ src/common/components/Routing/RoutingApp.tsx
@@ -4,7 +4,6 @@ import {PageOne} from "common/components/Routing/Pages/PageOne.tsx";
 import {PageTwo} from "common/components/Routing/Pages/PageTwo.tsx";
 import {PageThree} from "common/components/Routing/Pages/PageThree.tsx";
 import {Error404} from "common/components/Routing/Error/Error404.tsx";
-import {sstl} from "common/components/Routing/_styles.ts";
 
 export const RoutingApp = () => {
 
@@ -13,15 +12,24 @@ export const RoutingApp = () => {
             <div className={stl.header}><h1>HEADER</h1></div>
             <div className={stl.body}>
                 <div className={stl.nav}>
-                    <sstl.NavWrapper>
-                        <NavLink to={"/Page1"}>Adidas</NavLink>
-                    </sstl.NavWrapper>
-                    <sstl.NavWrapper>
-                        <NavLink to={"/Page2"}>Bershka</NavLink>
-                    </sstl.NavWrapper>
-                    <sstl.NavWrapper>
-                        <NavLink to={"/Page3"}>Pull&Bear</NavLink>
-                    </sstl.NavWrapper>
+                    <div>
+                        <NavLink
+                            className={({isActive}) => isActive ? stl.activeNavLink : stl.navLink}
+                            to={"/Page1"}
+                        >Adidas</NavLink>
+                    </div>
+                    <div>
+                        <NavLink
+                            className={({isActive}) => isActive ? stl.activeNavLink : stl.navLink}
+                            to={"/Page2"}
+                        >Bershka</NavLink>
+                    </div>
+                    <div>
+                        <NavLink
+                            className={({isActive}) => isActive ? stl.activeNavLink : stl.navLink}
+                            to={"/Page3"}
+                        >Pull&Bear</NavLink>
+                    </div>
                     <div>
                         <Link to={"/Page3"}>Pull&Bear with tag Link</Link>
                     </div>
