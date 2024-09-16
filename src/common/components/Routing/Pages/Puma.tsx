ec7da2dd0584c66223555bb1b7682b695f5a0daa
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-17 02:37:24 +0300

    Refactor JSX structure and fix text blocks
    Converted inappropriate use of `<p>` elements containing headers (i.e., `<h3>` inside `<p>`) into proper JSX structure. Adjusted the price value for an item in PB.tsx to reflect the correct amount.

--- src/common/components/Routing/Pages/Puma.tsx
+++ src/common/components/Routing/Pages/Puma.tsx
@@ -46,18 +46,20 @@ export const Puma: FC = () => {
                 }
             </div>
             <div className={stl.textInfo}>
+                <h3>What is Lorem Ipsum?</h3>
                 <p>
-                    <h3>What is Lorem Ipsum?</h3>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-                    printer
-                    took a galley of type and scrambled it to make a type specimen book.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                     It has survived not only five centuries, but also the leap into electronic typesetting,
-                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                    sheets
                     containing Lorem Ipsum passages, and more recently with desktop publishing software like
                     Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
 
-                    <h3>Why do we use it?</h3>
+                <h3>Why do we use it?</h3>
+                <p>
                     It is a long established fact that a reader will be distracted by the readable content of a page
                     when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
                     distribution of letters, as opposed to using 'Content here, content here', making it look like
@@ -66,8 +68,10 @@ export const Puma: FC = () => {
                     their
                     infancy. Various versions have evolved over the years, sometimes by accident,
                     sometimes on purpose (injected humour and the like).
+                </p>
 
-                    <h3>Where does it come from?</h3>
+                <h3>Where does it come from?</h3>
+                <p>
                     Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
                     classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,
                     a Latin professor at Hampden-Sydney College in Virginia, looked up one of the
