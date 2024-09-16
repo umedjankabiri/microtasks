4d85eec0a7b1cd808b8a280032db8d2580f492ee
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-16 22:03:55 +0300

    Move RoutingApp to Pages directory
    Updated import path for RoutingApp in main.tsx to reflect its new location under the Pages directory. This change is to improve the file structure and organization of the project.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
-import {RoutingApp} from "common/components/Routing/RoutingApp.tsx";
+import {RoutingApp} from "common/components/Routing/Pages/RoutingApp.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
