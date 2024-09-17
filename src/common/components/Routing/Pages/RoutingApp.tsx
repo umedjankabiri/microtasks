0ba9d6c32e66014b332ffb4700ffff3925978916
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-17 03:48:46 +0300

    Rename 'Model' to 'Brand' and update paths
    Modified the `RoutingApp.tsx` to replace 'Model' component with 'Brand' and updated associated routes. Standardized import naming and streamlined dynamic brand routing by consolidating it into a single route.

--- src/common/components/Routing/Pages/RoutingApp.tsx
+++ src/common/components/Routing/Pages/RoutingApp.tsx
@@ -1,17 +1,17 @@
-import stl from "common/components/Routing/Pages/styles/RoutingApp.module.css"
+import stl from "common/components/Routing/Pages/styles/routingApp.module.css"
 import {Navigate, NavLink, Route, Routes} from "react-router-dom";
 import {Adidas} from "common/components/Routing/Pages/Adidas.tsx";
 import {PB} from "common/components/Routing/Pages/PB.tsx";
 import {Puma} from "common/components/Routing/Pages/Puma.tsx";
 import {Error404} from "common/components/Routing/Error/Error404.tsx";
-import {Model} from "common/components/Routing/Pages/Model.tsx";
+import {Brand} from "common/components/Routing/Pages/Brand.tsx";
 
 export const PATH = {
     ID: "/:id",
     ADIDAS: "/adidas",
     PUMA: "/puma",
     PB: "/pb",
-    MODEL: "/model",
+    BRAND: "/brand",
     ERROR404: "/error404"
 } as const
 
@@ -47,9 +47,7 @@ export const RoutingApp = () => {
                         <Route path={PATH.ADIDAS} element={<Adidas/>}/>
                         <Route path={PATH.PUMA} element={<Puma/>}/>
                         <Route path={PATH.PB} element={<PB/>}/>
-                        <Route path={`${PATH.ADIDAS}${PATH.MODEL}${PATH.ID}`} element={<Model brands={"adidas"}/>}/>
-                        <Route path={`${PATH.PUMA}${PATH.MODEL}${PATH.ID}`} element={<Model brands={"puma"}/>}/>
-                        <Route path={`${PATH.PB}${PATH.MODEL}${PATH.ID}`} element={<Model brands={"pb"}/>}/>
+                        <Route path={`/:brands/:id`} element={<Brand />}/>
 
                         <Route path={PATH.ERROR404} element={<Error404/>}/>
                         <Route path={"/*"} element={<Error404/>}/>
