46a684ed7d38f627a329371c7538de120a4febd8
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-13 23:52:26 +0300

    Rename 'name' to 'collection' in StudentProps.
    This change aims to better reflect the purpose of the property, improving code clarity. It affects how student data is structured and accessed in the application.

--- src/App.tsx
+++ src/App.tsx
@@ -14,17 +14,17 @@ import {Input} from "common/components/Input/Input.tsx";
 function App() {
     /* nested-component */
     const students: StudentProps[] = [
-        {id: 1, name: "James", age: 8},
-        {id: 2, name: "Robert", age: 18},
-        {id: 3, name: "John", age: 28},
-        {id: 4, name: "Michael", age: 38},
-        {id: 5, name: "William", age: 48},
-        {id: 6, name: "David", age: 58},
-        {id: 7, name: "Richard", age: 68},
-        {id: 8, name: "Joseph", age: 78},
-        {id: 9, name: "Thomas", age: 88},
-        {id: 10, name: "Charles", age: 98},
-        {id: 11, name: "Christopher", age: 100},
+        {id: 1, collection: "James", age: 8},
+        {id: 2, collection: "Robert", age: 18},
+        {id: 3, collection: "John", age: 28},
+        {id: 4, collection: "Michael", age: 38},
+        {id: 5, collection: "William", age: 48},
+        {id: 6, collection: "David", age: 58},
+        {id: 7, collection: "Richard", age: 68},
+        {id: 8, collection: "Joseph", age: 78},
+        {id: 9, collection: "Thomas", age: 88},
+        {id: 10, collection: "Charles", age: 98},
+        {id: 11, collection: "Christopher", age: 100},
     ]
 
     /* button component */
