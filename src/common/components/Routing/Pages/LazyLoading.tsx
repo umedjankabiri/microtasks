6e3fc742d7cd5705b8a39ca8c071d037354dc6fe
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-13 21:05:20 +0300

    Add lazy loading for main brand pages
    Implement lazy loading for Adidas, PB, Puma, and Prices routes using React's `lazy` and `Suspense` components. This change optimizes the initial load time by deferring the loading of specific brand pages until they are needed. Additionally, a fallback loading indicator is displayed while the corresponding page is being loaded.

---
+++ src/common/components/Routing/Pages/LazyLoading.tsx
@@ -0,0 +1,56 @@
+import {createBrowserRouter} from "react-router-dom";
+import {PATH, RoutingApp} from "common/components/Routing/Pages/RoutingApp.tsx";
+import {Error404} from "common/components/Routing/Error/Error404.tsx";
+import React, {lazy, Suspense} from "react";
+
+const Adidas = lazy(() =>
+    import('common/components/Routing/Pages/Adidas') as unknown as Promise<{ default: React.ComponentType<any> }>);
+const PB = lazy(() =>
+    import('common/components/Routing/Pages/PB') as unknown as Promise<{ default: React.ComponentType<any> }>);
+const Puma = lazy(() =>
+    import('common/components/Routing/Pages/Puma') as unknown as Promise<{ default: React.ComponentType<any> }>);
+const Prices = lazy(() =>
+    import('common/components/Routing/Pages/Prices') as unknown as Promise<{ default: React.ComponentType<any> }>);
+
+export const LazyRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <RoutingApp/>,
+        errorElement: <Error404/>,
+        children: [
+            {
+                path: PATH.ADIDAS,
+                element: (
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Adidas/>
+                    </Suspense>
+                )
+            },
+            {
+                path: PATH.PB,
+                element: (
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <PB/>
+                    </Suspense>
+                )
+            },
+            {
+                path: PATH.PUMA,
+                element: (
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Puma/>
+                    </Suspense>
+                )
+            },
+            {
+                path: PATH.PRICES,
+                element: (
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Prices/>
+                    </Suspense>
+                )
+            }
+        ]
+    }
+]);
+// useNavigate, useMatches, useRouteError
