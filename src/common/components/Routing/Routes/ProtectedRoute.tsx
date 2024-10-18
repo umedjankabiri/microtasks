18dc6fd6113d83f2aa8a06d3b50b4d7c88ef9538
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-18 17:39:53 +0300

    Refactor ProtectedRoute component
    Simplify the ProtectedRoute component by removing unnecessary props and using Outlet for nested routing. The authorization check directly redirects to the login page if the user is not authenticated.

--- src/common/components/Routing/Routes/ProtectedRoute.tsx
+++ src/common/components/Routing/Routes/ProtectedRoute.tsx
@@ -1,19 +1,8 @@
-import {ReactNode} from "react";
-import { Navigate } from "react-router-dom";
+import {Navigate, Outlet} from "react-router-dom";
 import {PATH} from "common/components/Routing/RoutingApp.tsx";
 
-type ProtectedRouteProps = {
-    children: ReactNode;
-}
+export const ProtectedRoute = () => {
+    const isAuth: boolean = false;
 
-export const ProtectedRoute = (props: ProtectedRouteProps) => {
-    const isLoading = props.children
-        ? props.children
-        : <Navigate to={PATH.ERROR404}/>
-
-    return (
-        <div>
-            {isLoading}
-        </div>
-    );
+    return isAuth ? <Outlet/> : <Navigate to={PATH.LOGIN}/>
 };
