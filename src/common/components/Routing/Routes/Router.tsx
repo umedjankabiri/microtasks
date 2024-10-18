b2736adaaaeb4db76ea8ef38a1c5d60ea5bf89d9
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-18 17:40:05 +0300

    Restructure routing and add login page
    Separated public and private routes for better organization and extendability. Introduced the Login component and updated error handling to use Navigate for redirecting to a 404 page. This change aims to enhance code clarity and future scalability of the routing system.

--- src/common/components/Routing/Routes/Router.tsx
+++ src/common/components/Routing/Routes/Router.tsx
@@ -1,56 +1,62 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate, RouteObject} from "react-router-dom";
 import {PATH, RoutingApp} from "common/components/Routing/RoutingApp.tsx";
 import {Error404} from "common/components/Routing/Pages/Error/Error404.tsx";
 import {Adidas} from "common/components/Routing/Pages/Adidas.tsx";
 import {PB} from "common/components/Routing/Pages/PB.tsx";
 import {Puma} from "common/components/Routing/Pages/Puma.tsx";
 import {Prices} from "common/components/Routing/Pages/Prices.tsx";
-import {Brand} from "common/components/Routing/Pages/Brand.tsx";
 import {ProtectedPage} from "common/components/Routing/Pages/ProtectedPage.tsx";
+import {Login} from "common/components/Routing/Pages/Login.tsx";
 import {ProtectedRoute} from "common/components/Routing/Routes/ProtectedRoute.tsx";
+
+const publicRoutes: RouteObject[] = [
+    {
+        path: "/",
+        element: <Adidas/>
+    },
+    {
+        path: PATH.ADIDAS,
+        element: <Adidas/>
+    },
+    {
+        path: PATH.PB,
+        element: <PB/>
+    },
+    {
+        path: PATH.PUMA,
+        element: <Puma/>
+    },
+    {
+        path: PATH.PRICES,
+        element: <Prices/>
+    },
+    {
+        path: PATH.LOGIN,
+        element: <Login/>
+    },
+    {
+        path: PATH.ERROR404,
+        element: <Error404/>
+    }
+]
+const privateRoutes: RouteObject[] = [
+    {
+        path: PATH.PROTECTED_PAGE,
+        element: <ProtectedPage/>
+    },
+]
 
 export const Router = createBrowserRouter([
     {
         path: "/",
         element: <RoutingApp/>,
-        errorElement: <Error404/>,
+        errorElement: <Navigate to={PATH.ERROR404} />,
         children: [
             {
-                path: "/",
-                element: <Adidas/>
+                element: <ProtectedRoute/>,
+                children: privateRoutes
             },
-            {
-                path: PATH.ADIDAS,
-                element: <Adidas/>
-            },
-            {
-                path: PATH.PB,
-                element: <PB/>
-            },
-            {
-                path: PATH.PUMA,
-                element: <Puma/>
-            },
-            {
-                path: PATH.PROTECTED_PAGE,
-                element: (
-                    <ProtectedRoute>
-                        <ProtectedPage/>
-                    </ProtectedRoute>
-                ),
-            },
-            {
-                path: PATH.PRICES,
-                element: <Prices/>
-            },
-            {
-                path: `${PATH.BRAND}${PATH.ID}`,
-                element: <Brand/>
-            },
-            {
-                path: '/*',
-                element: <Error404/>
-            }
+            ...publicRoutes,
         ]
     }
 ])
